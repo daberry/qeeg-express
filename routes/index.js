e496648931978995c39d750575c0f34cda79f296
David Berry <daberrydsm@gmail.com>
2017-09-13 08:03:56 -0700

    consolidation of front-end views and components

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,16 @@ var qeegLib = require('../lib/qeeg-lib');
 
 
 /* GET home page. */
-router.get('/index', function(req, res, next) {
-
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/qeeg/data', function(req,res,next) {
-
-
   res.status(200).send(qeegLib.qeegParse('Russo 21_01_16 14_36_18 00.42 to 20.03.rec'));
+});
+
+router.get('/qeeg/fft', function(req, res, next) {
+  res.status(200).send(qeegLib.fft('Russo 21_01_16 14_36_18 00.42 to 20.03.rec'));
 });
 
 
